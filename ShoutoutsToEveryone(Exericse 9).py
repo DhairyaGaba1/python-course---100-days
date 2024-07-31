f075342ca5276beb08ef0d77ec829ca868ab1ca7
Alpha4210 <alpha.gamer.2427@gmail.com>
2024-07-31 20:09:38 +0530

    Exercise 9 completed

--- ShoutoutsToEveryone(Exericse 9).py
+++ ShoutoutsToEveryone(Exericse 9).py
@@ -1,9 +1,15 @@
 # Write a program to pronounce list of names using win32 API. If you are given a list l as follows:
 
-# l = ["Rahul", "Nishant", "Harry"]
 # Your program should pronouce:
 
 # Shoutout to Rahul
 # Shoutout to Nishant
 # Shoutout to Harry
 
+import win32com.client 
+
+l = ["Rahul", "Nishant", "Harry"]
+speaker = win32com.client.Dispatch("SAPI.SpVoice") 
+for i in l:
+    print(f"shoutout to {i}")
+    speaker.speak(f"Shoutout to {i}")
